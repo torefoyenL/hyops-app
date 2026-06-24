a36d67474d74024694e0337b77bd01fac2ac22f7
torefoyenL <torefoyen@gmail.com>
2026-06-24 21:08:09 +0200

    Update plant_operations.py

--- plant_operations.py
+++ plant_operations.py
@@ -12,7 +12,8 @@ class ContainerType:
     fleet_fraction: float
 
 
-def adiabatic_compression_energy(mass_kg, P1_bar=30, P2_bar=380, T=298):
+def adiabatic_compression_energy(mass_kg, P1_bar=30, P2_bar=380, T=298,
+                                  Isentropic_efficiency=0.4):
     """Returns energy in kWh to compress H2 mass from P1 to P2 adiabatically."""
     if mass_kg <= 0:
         return 0.0
@@ -23,7 +24,7 @@ def adiabatic_compression_energy(mass_kg, P1_bar=30, P2_bar=380, T=298):
     P1    = P1_bar * 1e5
     P2    = P2_bar * 1e5
     work_joules = (n * R * T) / (gamma - 1) * ((P2 / P1)**((gamma - 1) / gamma) - 1)
-    return work_joules / 3.6e6
+    return (work_joules / 3.6e6) / Isentropic_efficiency
 
 
 class Container:
